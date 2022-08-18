8972abb4f48352defa8e97c50b8cb2f8dad935f4
SusheendharVijay <susheendharvj@gmail.com>
2022-08-18 13:27:19 +0530

    🛠  Fix: (ignoring token details while getting gas tank info)

--- lib/types.ts
+++ lib/types.ts
@@ -2,10 +2,6 @@ import { z } from "zod";
 
 export const WalletInfoSchema = z.object({
   pda: z.string(),
-  tokenName: z.string(),
-  tokenMint: z.string(),
-  tokenDecimals: z.number(),
   gasTankPublicKey: z.string(),
   gasTankSecretKey: z.string(),
-  created: z.string(),
 });
